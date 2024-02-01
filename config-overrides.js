d2aca066dc4548a2c491c7ea915439ed679d74b8
GiordanoT <giordanotinella@gmail.com>
2024-02-01 12:37:15 +0100

    tailwind configuration

--- config-overrides.js
+++ config-overrides.js
@@ -1,5 +1,5 @@
 module.exports = function override(config, env) {
-    config.mode = 'production';
+    config.mode = 'development';
     config.optimization = {minimize: true};
     return config;
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import {GenericProps} from './common/types';
+import {useEffectOnce} from 'usehooks-ts';
+import {useState} from "react";
 
 type Props = GenericProps & {};
 function App(props: Props) {
-  return (<div>
-    Hello World
+  const  [data, setData] = useState<JSON|null>(null);
+  useEffectOnce(() => {
+    (async function() {
+      const response = await fetch('./data.json');
+      if(response.ok) setData(await response.json());
+    })();
+  });
+
+  if(!data) return(<div className={'bg-red-300 p-2'}>Loading...</div>)
+  return (<div className={'bg-red-300 p-2'}>
+    {JSON.stringify(data)}
   </div>);
 }
 
